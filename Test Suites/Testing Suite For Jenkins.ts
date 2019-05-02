9d97b458b45c5ab393de4d5ce8f76e0660f26232
bappyasif <bappyasif@hotmail.com>
2019-05-02 21:17:01 +0600

    Test Commit From Jenkins

---
+++ Test Suites/Testing Suite For Jenkins.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>Just Trynig Jenkins Git Integration Test from CMD.</description>
+   <name>Testing Suite For Jenkins</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>65870171-8ebb-40bc-9893-f535e6bb3d78</testSuiteGuid>
+   <testCaseLink>
+      <guid>eb4bc675-b2d4-41b3-952e-c6433ca7db3f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Fake Sample</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
